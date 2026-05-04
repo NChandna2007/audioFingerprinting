80102d738a678fdb732f37a3921fbcc25048ceab
Nehal Chandna <nehalchandna@gmail.com>
2026-05-04 16:53:23 +0530

    fingperint

--- fingerprint.py
+++ fingerprint.py
@@ -14,25 +14,23 @@ def get_freq_band(freq_bin, n_bands=12):
     return min(int(math.log2(freq_bin / 10) * 3), n_bands - 1)
 
 def generate_hashes(filepath):
-    # 1. load audio
+
     sample_rate, signal = wavfile.read(filepath)
 
-    # 2. mono conversion
     if len(signal.shape) == 2:
         signal = signal.mean(axis=1)
 
-    # 3. normalize
     signal = signal.astype(np.float32) / 32767.0
 
-    # 4. STFT
+
     f, t, Zxx = stft(signal, fs=sample_rate, nperseg=1024)
 
-    # 5. spectrogram
+
     spec = np.abs(Zxx)
     log_spec = np.log1p(spec)
     log_spec = log_spec / (np.max(log_spec) + 1e-6)
 
-    # 6. peak detection
+
     neighborhood_size = (30, 10)
     local_max = maximum_filter(log_spec, size=neighborhood_size) == log_spec
     threshold = np.mean(log_spec) + 0.5 * np.std(log_spec)
@@ -44,10 +42,10 @@ def generate_hashes(filepath):
     peaks = sorted(peaks, key=lambda x: -x[2])
     peaks = peaks[:MAX_PEAKS]
 
-    # 7. sort by time for hashing
+
     peaks_by_time = sorted(peaks, key=lambda x: x[1])
 
-    # 8. hashing
+
     hashes = []
     for i in range(len(peaks_by_time)):
         f1, t1, _ = peaks_by_time[i]
